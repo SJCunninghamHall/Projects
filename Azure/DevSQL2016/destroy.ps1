46d7e675c3c297f2a1d04a0dbf7c0ae15292fa55
Stephen Cunningham <51404147+SJCunninghamHall@users.noreply.github.com>
2019-06-18 15:52:06 +0100

    Added a comment

--- Azure/DevSQL2016/destroy.ps1
+++ Azure/DevSQL2016/destroy.ps1
@@ -1,1 +1,2 @@
+# Get rid of the resource group to remove all artifacts associated with it, thereby removing the server completely. Use the RG name supplied in the deploy.ps1 script.
 Remove-AzResourceGroup -Name devRG
