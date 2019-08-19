dd74e4fea0a368dda33f6d011bed53174ab2941f
Stephen Cunningham <51404147+SJCunninghamHall@users.noreply.github.com>
2019-08-19 11:08:22 +0100

    WIP

---
+++ Chops/Chops/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Chops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOrig = new System.Windows.Forms.TextBox();
+            this.txtChopped = new System.Windows.Forms.TextBox();
+            this.btnChop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOrig
+            // 
+            this.txtOrig.Location = new System.Drawing.Point(13, 13);
+            this.txtOrig.Multiline = true;
+            this.txtOrig.Name = "txtOrig";
+            this.txtOrig.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOrig.Size = new System.Drawing.Size(659, 173);
+            this.txtOrig.TabIndex = 0;
+            this.txtOrig.Text = "The quick brown fox jumps over the lazy dog";
+            // 
+            // txtChopped
+            // 
+            this.txtChopped.Location = new System.Drawing.Point(13, 192);
+            this.txtChopped.Multiline = true;
+            this.txtChopped.Name = "txtChopped";
+            this.txtChopped.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtChopped.Size = new System.Drawing.Size(659, 173);
+            this.txtChopped.TabIndex = 1;
+            // 
+            // btnChop
+            // 
+            this.btnChop.Location = new System.Drawing.Point(305, 371);
+            this.btnChop.Name = "btnChop";
+            this.btnChop.Size = new System.Drawing.Size(75, 23);
+            this.btnChop.TabIndex = 2;
+            this.btnChop.Text = "&Chop";
+            this.btnChop.UseVisualStyleBackColor = true;
+            this.btnChop.Click += new System.EventHandler(this.btnChop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 405);
+            this.Controls.Add(this.btnChop);
+            this.Controls.Add(this.txtChopped);
+            this.Controls.Add(this.txtOrig);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOrig;
+        private System.Windows.Forms.TextBox txtChopped;
+        private System.Windows.Forms.Button btnChop;
+    }
+}
+
---
+++ Chops/Chops/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChop_Click(object sender, EventArgs e)
+        {
+            string toBeChopped = txtOrig.Text;
+
+            if (toBeChopped.Trim() == string.Empty)
+            {
+                MessageBox.Show("Enter text to be chopped");
+                return;
+            }
+
+            List<string> breakDown = toBeChopped.Split(' ').ToList();
+
+            int numberOfWords = breakDown.Count();
+
+            breakDown.CutUp();
+
+            breakDown.Shuffle();
+
+            txtChopped.Clear();
+
+            txtChopped.AppendText(string.Join(" ", breakDown.GetRange(0, numberOfWords)));
+
+            return;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic]
+        private static Random Local;
+
+        public static Random ThisThreadsRandom
+        {
+            get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+        }
+    }
+
+    static class MyExtensions
+    {
+
+        public static void CutUp<T>(this IList<T> list)
+        {
+            // Create a master list consisting of one or more lists of sections of the original text
+
+            // Create storage for temp work lists and collection of those lists
+            List<List<T>> listOfLists = new List<List<T>>();
+            List<T> scratchList = new List<T>();
+            
+            // Work out the maximum number of words to keep together in the event of that decision being reached
+
+            int n = list.Count;
+            int scanCounter = 0;
+            int fullN = n;
+            int maxChunk;
+            int chunkProgress = 0;
+
+            if (fullN >= 2)
+            {
+                maxChunk = fullN / 2;
+            }
+            else
+            {
+                maxChunk = 1;
+            }
+
+            foreach (var word in list)
+            {
+                // Determine how many words to add to the current list
+
+                // Skip this many words
+                if (chunkProgress == 0) // Just finished a cycle or one didn't exist in the first place
+                {
+                    chunkProgress = ThreadSafeRandom.ThisThreadsRandom.Next(1, maxChunk);
+                }
+
+                scratchList.Add(word);
+
+                if (chunkProgress > 0)
+                {
+                    chunkProgress--;
+                }
+                
+                // Check if we're done with this chunk of words
+                if (chunkProgress == 0)
+                {
+                    listOfLists.Add(new List<T>(scratchList));
+                    scratchList.Clear();
+                }
+            }
+
+            // It's possible we have reached the last word in the list and dropped out of the loop
+            // before adding the final scratch list to the master list. Check and add
+
+            //while (n > 1)
+            //{
+
+            //    // Determine how many words to add to the current list
+
+            //    // Skip this many words
+            //    chunkProgress = ThreadSafeRandom.ThisThreadsRandom.Next(1, maxChunk);
+            //    scratchList.Add(list[n]);
+            //    if (chunkProgress > 0)
+            //    {
+            //        chunkProgress--;
+            //    }
+            //    else
+            //    {
+            //        listOfLists.Add(scratchList);
+            //        scratchList.Clear();
+            //    }
+
+            //    n--;
+            //}
+
+        }
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            int fullN = n;
+
+            // Work the master list from start to finish
+            while (n > 1)
+            {
+                // n--;
+
+                    int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+
+                    T value = list[k];
+                    list[k] = list[n];
+                    list[n] = value;
+
+                    n--;
+            }
+        }
+    }
+
+
+
+}
