9c05a014ac2749774a12d7ae7dea8f337897582b
Stephen Cunningham <51404147+SJCunninghamHall@users.noreply.github.com>
2019-08-20 15:10:06 +0100

    wip

--- Chops/Chops/Form1.Designer.cs
+++ Chops/Chops/Form1.Designer.cs
@@ -29,32 +29,12 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCUT));
-            this.txtOrig = new System.Windows.Forms.TextBox();
-            this.txtChopped = new System.Windows.Forms.TextBox();
             this.btnChop = new System.Windows.Forms.Button();
             this.nudMaxConsec = new System.Windows.Forms.NumericUpDown();
+            this.txtSource = new System.Windows.Forms.RichTextBox();
+            this.txtChoppeds = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.nudMaxConsec)).BeginInit();
             this.SuspendLayout();
-            // 
-            // txtOrig
-            // 
-            this.txtOrig.Location = new System.Drawing.Point(13, 13);
-            this.txtOrig.Multiline = true;
-            this.txtOrig.Name = "txtOrig";
-            this.txtOrig.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtOrig.Size = new System.Drawing.Size(659, 173);
-            this.txtOrig.TabIndex = 0;
-            this.txtOrig.Text = resources.GetString("txtOrig.Text");
-            this.txtOrig.Leave += new System.EventHandler(this.txtOrig_LostFocus);
-            // 
-            // txtChopped
-            // 
-            this.txtChopped.Location = new System.Drawing.Point(13, 192);
-            this.txtChopped.Multiline = true;
-            this.txtChopped.Name = "txtChopped";
-            this.txtChopped.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtChopped.Size = new System.Drawing.Size(659, 173);
-            this.txtChopped.TabIndex = 1;
             // 
             // btnChop
             // 
@@ -73,30 +53,44 @@
             this.nudMaxConsec.Size = new System.Drawing.Size(54, 20);
             this.nudMaxConsec.TabIndex = 3;
             // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(13, 13);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(659, 173);
+            this.txtSource.TabIndex = 4;
+            this.txtSource.Text = resources.GetString("txtSource.Text");
+            // 
+            // txtChoppeds
+            // 
+            this.txtChoppeds.Location = new System.Drawing.Point(13, 192);
+            this.txtChoppeds.Name = "txtChoppeds";
+            this.txtChoppeds.Size = new System.Drawing.Size(659, 173);
+            this.txtChoppeds.TabIndex = 5;
+            this.txtChoppeds.Text = "";
+            // 
             // frmCUT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(738, 405);
+            this.Controls.Add(this.txtChoppeds);
+            this.Controls.Add(this.txtSource);
             this.Controls.Add(this.nudMaxConsec);
             this.Controls.Add(this.btnChop);
-            this.Controls.Add(this.txtChopped);
-            this.Controls.Add(this.txtOrig);
             this.Name = "frmCUT";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "C.U.T";
             ((System.ComponentModel.ISupportInitialize)(this.nudMaxConsec)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtOrig;
-        private System.Windows.Forms.TextBox txtChopped;
         private System.Windows.Forms.Button btnChop;
         private System.Windows.Forms.NumericUpDown nudMaxConsec;
+        private System.Windows.Forms.RichTextBox txtSource;
+        private System.Windows.Forms.RichTextBox txtChoppeds;
     }
 }
 
--- Chops/Chops/Form1.cs
+++ Chops/Chops/Form1.cs
@@ -19,9 +19,9 @@ namespace Chops
         {
             InitializeComponent();
             this.nudMaxConsec.Minimum = 1;
-            if (this.txtOrig.Text.Length > 2)
+            if (this.txtSource.Text.Length > 2)
             {
-                this.nudMaxConsec.Maximum = txtOrig.Text.Length / 2;
+                this.nudMaxConsec.Maximum = txtSource.Text.Length / 2;
                 this.nudMaxConsec.Value = 3;
             }
             else
@@ -34,7 +34,7 @@ namespace Chops
 
         private void btnChop_Click(object sender, EventArgs e)
         {
-            string toBeChopped = txtOrig.Text;
+            string toBeChopped = txtSource.Text;
 
             if (toBeChopped.Trim() == string.Empty)
             {
@@ -55,20 +55,41 @@ namespace Chops
 
             // breakDown.Shuffle();
 
-            txtChopped.Clear();
+            txtChoppeds.Clear();
 
-            txtChopped.AppendText(string.Join(" ", breakDown.GetRange(0, numberOfWords)));
+            txtChoppeds.AppendText(string.Join(" ", breakDown.GetRange(0, numberOfWords)));
 
             return;
         }
 
 
+        private void txtSource_LostFocus(object sender, EventArgs e)
+        {
+            if (this.txtSource.Text.Length > 2)
+            {
+                this.nudMaxConsec.Maximum = txtSource.Text.Length / 2;
+                if (this.txtSource.Text.Length >= 3)
+                {
+                    this.nudMaxConsec.Value = 3;
+                }
+                else
+                {
+                    this.nudMaxConsec.Value = 1;
+                }
+            }
+            else
+            {
+                this.nudMaxConsec.Maximum = 1;
+                this.nudMaxConsec.Value = 1;
+            }
+        }
+
         private void txtOrig_LostFocus(object sender, EventArgs e)
         {
-            if (this.txtOrig.Text.Length > 2)
+            if (this.txtSource.Text.Length > 2)
             {
-                this.nudMaxConsec.Maximum = txtOrig.Text.Length / 2;
-                if (this.txtOrig.Text.Length >= 3)
+                this.nudMaxConsec.Maximum = txtSource.Text.Length / 2;
+                if (this.txtSource.Text.Length >= 3)
                 {
                     this.nudMaxConsec.Value = 3;
                 }
