61f216edc93fee9a446ea9c92d3e54dc7547e661
Stephen Cunningham <51404147+SJCunninghamHall@users.noreply.github.com>
2019-08-29 12:19:11 +0100

    Update Program.cs

--- PolyTest/PolyTest/Program.cs
+++ PolyTest/PolyTest/Program.cs
@@ -42,6 +42,21 @@ namespace PolymorphismApplication
             return (width * height);
         }
     }
+
+    class Parallelagram : Shape
+    {
+        public Parallelagram(int a = 0, int b = 0) : base(a, b)
+        {
+            Console.WriteLine("Width inherited from base class: {0}", width);
+            Console.WriteLine("Height inherited from base class: {0}", height);
+        }
+
+        public override int area()
+        {
+            Console.WriteLine("Parallelagram class area :");
+            return (width * height);
+        }
+    }
     class Triangle : Shape
     {
         public Triangle(int a = 0, int b = 0) : base(a, b)
@@ -62,18 +77,35 @@ namespace PolymorphismApplication
             Console.WriteLine("Area: {0}", a);
         }
     }
+
+    class CallerSS
+    {
+        public void CallAreaSS(Parallelagram pss)
+        {
+            int pa;
+            pa = pss.area();
+            Console.WriteLine("Parallelagram area: {0}", pa);
+        }
+    }
     class Tester
     {
         static void Main(string[] args)
         {
             Caller c = new Caller();
+            CallerSS css = new CallerSS();
             Rectangle r = new Rectangle(10, 7);
             Triangle t = new Triangle(10, 5);
             Square s = new Square(6, 6);
+            Parallelagram p = new Parallelagram(31415, 66261);
+
 
             c.CallArea(r);
             c.CallArea(t);
             c.CallArea(s);
+            c.CallArea(p);
+
+            css.CallAreaSS(p);
+
             Console.ReadKey();
         }
     }
