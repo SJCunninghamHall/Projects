4894b43b07d20e7117a7cbfeca8c8ec06fec8960
Stephen Cunningham <51404147+SJCunninghamHall@users.noreply.github.com>
2019-09-02 15:13:44 +0100

    WIP

---
+++ PolyTest15/PolyTest15/Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyTest15
+{
+    class Shape
+    {
+        protected int width, height;
+
+        public Shape(int a = 0, int b = 0)
+        {
+            width = a;
+            height = b;
+        }
+
+        public virtual int Area()
+        {
+            Console.WriteLine($"Parent class area :");
+            return 0;
+        }
+
+    }
+}
