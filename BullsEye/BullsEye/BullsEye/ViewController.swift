3d95d667b1673c5b95ef716c74da4cbae19b4f66
Stephen Cunningham <stephen@cunninghamhall.co.uk>
2019-09-16 19:06:30 +0100

    WIP

--- BullsEye/BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/BullsEye/ViewController.swift
@@ -28,6 +28,28 @@ class ViewController: UIViewController
         targetValue = 0
         
         startNewRound()
+        
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+
+        
     }
     
     override func viewDidLoad()
